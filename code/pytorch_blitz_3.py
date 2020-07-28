dd2bb2b92346ac157f9517ea6534da80c107e120
suryadheeshjith <Surya.dheeshjith@gmail.com>
2020-07-28 19:18:24 +0530

    Added files

--- code/pytorch_blitz_3.py
+++ code/pytorch_blitz_3.py
@@ -3,6 +3,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# To print backward graph
+def print_graph(g, level=0):
+    if g == None: return
+    print('*'*level*2, g)
+    for subg in g.next_functions:
+        print_graph(subg[0], level+1)
+
 # Class for NN
 class Net(nn.Module):
 
@@ -13,7 +20,7 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(16*6*6, 120)
         self.fc2 = nn.Linear(120,84)
         self.fc3 = nn.Linear(84,10)
-        
+
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
         x = F.max_pool2d(F.relu(self.conv2(x)),2)
@@ -22,15 +29,15 @@ class Net(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-        
+
     def num_flat_features(self, x):
         size = x.size()[1:]  # all dimensions except the batch dimension
         num_features = 1
         for s in size:
             num_features *= s
         return num_features
-        
-    
+
+
 net = Net()
 print(net)
 
@@ -64,6 +71,9 @@ print(loss.grad_fn)  # MSELoss
 print(loss.grad_fn.next_functions[0][0])  # Linear
 print(loss.grad_fn.next_functions[0][0].next_functions[0][0])
 
+print("Printing Complete backward graph")
+print_graph(loss.grad_fn, 0)
+
 net.zero_grad()     # zeroes the gradient buffers of all parameters
 
 print('conv1.bias.grad before backward')
@@ -77,7 +87,7 @@ print(net.conv1.bias.grad)
 learning_rate = 0.01
 for f in net.parameters():
     f.data.sub_(f.grad.data * learning_rate)
-    
+
 import torch.optim as optim
 
 # create your optimizer
